d844ef9494586d93c56f867a6b041a4c3ce60b8e
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-09-30 15:45:35 -0600

    imported styled components/created Navbar styled component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,50 +1,51 @@
 import React from "react";
 
+// Style
+import { Navbar } from "./style.js";
 const Header = () => {
   return (
     <>
       <h1>Header</h1>
-      <header>
-        <nav class="navbar">
-          <a href="./index.html">
-            <img
-              src="./assets/logo--light.png"
-              class="navbar--logo"
-              alt="Hound Express logo"
+      {/* <Navbar > class="navbar" */}
+      <Navbar>
+        <a href="./index.html">
+          <img
+            src="./assets/logo--light.png"
+            class="navbar--logo"
+            alt="Hound Express logo"
+          />
+        </a>
+        <div class="navbar--right">
+          <ul>
+            <li>
+              <a href="#">Inicio</a>
+            </li>
+            <li>
+              <a href="#">Registro</a>
+            </li>
+            <li>
+              <a href="#">Estado General</a>
+            </li>
+            <li>
+              <a href="#">Lista</a>
+            </li>
+            <li>
+              <a href="#">Historial</a>
+            </li>
+          </ul>
+          <div class="search">
+            <button class="search__button" onclick="buscar()">
+              <img src="assets/search-icon.svg" alt="Buscar" />
+            </button>
+            <input
+              class="search__input"
+              id="search"
+              placeholder="Ingresar número de rastreo"
+              type="text"
             />
-          </a>
-          <div class="navbar--right">
-            <ul>
-              <li>
-                <a href="#">Inicio</a>
-              </li>
-              <li>
-                <a href="#">Registro</a>
-              </li>
-              <li>
-                <a href="#">Estado General</a>
-              </li>
-              <li>
-                <a href="#">Lista</a>
-              </li>
-              <li>
-                <a href="#">Historial</a>
-              </li>
-            </ul>
-            <div class="search">
-              <button class="search__button" onclick="buscar()">
-                <img src="assets/search-icon.svg" alt="Buscar" />
-              </button>
-              <input
-                class="search__input"
-                id="search"
-                placeholder="Ingresar número de rastreo"
-                type="text"
-              />
-            </div>
           </div>
-        </nav>
-      </header>
+        </div>
+      </Navbar>
     </>
   );
 };

79b9f2146ebb9c7f8673de4cfc97eb3bc460c251
Fernando Mejia Laguna <mejia190899@gmail.com>
2025-10-04 11:40:42 -0600

    added footer content

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
         <Header></Header>
         <Banner></Banner>
-        {/* <Body>  </Body> */}
+
         <Footer></Footer>
       </ThemeProvider>
     </>
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,47 @@
-import React from "react";
-
 const Footer = () => {
   return (
     <>
-      <h1>Footer</h1>
+      <footer>
+        <div class="wrapper">
+          <a class="footer-logo-link" href="./index.html">
+            <img
+              src="./assets/logo--light.png"
+              class="footer-logo"
+              alt="Hound Express logo"
+            />
+          </a>
+          <div class="text-container">
+            <div>
+              <p>
+                Copyright © 2025 Hound Express. Todos los derechos reservados.
+              </p>
+            </div>
+            <div>
+              <p>Contacto: +52(55) 4000 1920</p>
+            </div>
+            <div class="social-icons--container">
+              <img
+                class="social-icon"
+                src="./assets/facebook-icon.svg"
+                alt=""
+              />
+              <img
+                class="social-icon"
+                src="./assets/linkedin-icon.svg"
+                alt=""
+              />
+              <img
+                class="social-icon"
+                src="./assets/instagram-icon.svg"
+                alt=""
+              />
+            </div>
+            <div>
+              <p>Aviso de Privacidad</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
